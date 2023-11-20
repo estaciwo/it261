114845e64f9ce4b85738080eaa7c1fe539a80473
estaciwo <staciwo@gmail.com>
2023-11-20 15:47:52 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -76,6 +76,11 @@
                 <li><a href="weeks/week7/rand.php">rand.php</a></li>
                 <li><a href="weeks/week7/strings.php">strings.php</a></li>
             </ol>
+              <h3>Week 8</h3>
+            <ol>
+                <li><a href="weeks/week8/people.php">people.php</a></li>
+                <li><a href="weeks/week8/people-view.php">people-view.php</a></li>
+            </ol>
         </aside>
         <div id="screenshots">
             <div id="mamp"><h3>Screenshot 1: MAMP Installed</h3>
