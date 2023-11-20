765274bc3d523949f07c2b69ab1c21aa9368b64a
estaciwo <staciwo@gmail.com>
2023-11-20 15:47:33 -0800

    Add files via upload

---
+++ weeks/week8/credentials.php
@@ -0,0 +1,6 @@
+<?php  // my credentials that will allow me to connect to the database!!!
+
+define('DB_NAME', 'dbit261');
+define('DB_USER', 'estaciwo23');
+define('DB_PASSWORD', 'DX#34v3NDB');
+define('DB_HOST', 'mysql.estaciwo.com');
