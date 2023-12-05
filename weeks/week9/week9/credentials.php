e34148221eaa4a80a2acd170eb812f2e37d062c7
estaciwo <staciwo@gmail.com>
2023-12-04 21:59:28 -0800

    Delete weeks/week9/week9 directory

--- weeks/week9/week9/credentials.php
+++
@@ -1,6 +0,0 @@
-<?php  // my credentials that will allow me to connect to the database!!!
-
-define('DB_NAME', 'dbit261');
-define('DB_USER', 'estaciwo23');
-define('DB_PASSWORD', 'DX#34v3NDB');
-define('DB_HOST', 'mysql.estaciwo.com');
